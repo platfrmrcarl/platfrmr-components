763aa6d4eea527ea8026bd5cea17aabfe01336f3
carl Finch <carl@platfrmr.com>
2025-01-09 12:22:20 -0500

    started working on the UI Component library

---
+++ src/Button.jsx
@@ -0,0 +1,12 @@
+"use strict";
+"use client";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Button = void 0;
+const react_1 = __importDefault(require("react"));
+const Button = () => {
+    return (<button className='btn btn-primary'>Click Me!</button>);
+};
+exports.Button = Button;
---
+++ src/Button.tsx
@@ -0,0 +1,10 @@
+
+"use client";
+import React from 'react'
+
+export const Button = () => {
+  return (
+    <button className='btn btn-primary'>Click Me!</button>
+  )
+}
+
